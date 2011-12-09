faaf3f2eccf709e8848b44ac38a667b508e037ad
oxon <oxon@03b345a6-8283-4e7e-8394-dcfc2f156d9d>
2011-12-09 11:20:14 +0000

    - Fixed a bug in calculation of Fresnel refraction. Boundary check is now working properly, as in v1.0.0. - Implemented Fresnel reflection - Fixed a bug in calculation of the speed of light - Added DoFresnel and DoReflection for simplification

--- src/AOpticsManager.cxx
+++ src/AOpticsManager.cxx
@@ -26,10 +26,12 @@
 #endif
 #endif
 
+static const Double_t kEpsilon = 1e-6; // Fixed in TGeoNavigator.cxx (equiv to 1e-6 cm)
+
 ClassImp(AOpticsManager)
 
 //_____________________________________________________________________________
-AOpticsManager::AOpticsManager() : TGeoManager()
+AOpticsManager::AOpticsManager() : TGeoManager(), fDisableFresnelReflection(kFALSE)
 {
   fLimit = 100;
   fNThreads = 1;
@@ -40,7 +42,7 @@ AOpticsManager::AOpticsManager() : TGeoManager()
 
 //_____________________________________________________________________________
 AOpticsManager::AOpticsManager(const char* name, const char* title)
- : TGeoManager(name, title)
+ : TGeoManager(name, title), fDisableFresnelReflection(kFALSE)
 {
   fLimit = 100;
   fNThreads = 1;
@@ -55,25 +57,84 @@ AOpticsManager::~AOpticsManager()
 }
 
 //_____________________________________________________________________________
+void AOpticsManager::DoFresnel(Double_t n1, Double_t n2, ARay& ray)
+{
+  // Use the same notation used in Wikipedia
+  // http://en.wikipedia.org/wiki/Fresnel_equations
+  // theta_i = incident angle
+  // theta_t = transmission angle
+  Double_t* n = FindNormal(); // normal vect perpendicular to the surface
+  Double_t cosi = fD1[0]*n[0] + fD1[1]*n[1] + fD1[2]*n[2]; // cos(theta_i)
+  Double_t sini = TMath::Sqrt(1 - cosi*cosi);
+  Double_t sint = n1*sini/n2; // Snell's law
+
+  if(sint > 1.){ // total internal reflection
+    fStep -= kEpsilon*2.; // stop the step before reaching the boundary
+    DoReflection(n1, ray);
+    return;
+  } // if
+
+  Double_t cost = TMath::Sqrt(1 - sint*sint);
+
+  if(fDisableFresnelReflection == kFALSE){
+    Double_t Rs = TMath::Power((n1*cosi - n2*cost)/(n1*cosi + n2*cost), 2); // reflectivity for s-polarized photon
+    Double_t Rp = TMath::Power((n1*cost - n2*cosi)/(n1*cost + n2*cosi), 2); // reflectivity for p-polarized photon
+    Double_t R = (Rs + Rp)/2.; // We assume that polarization is random
+
+    if(gRandom->Uniform(1) < R){ // reflection at the boundary
+      fStep -= kEpsilon*2.; // stop the step before reaching the boundary
+      DoReflection(n1, ray);
+      return;
+    } // if
+  } // if
+
+  for(Int_t i = 0; i < 3; i++){
+    fX2[i] = fX1[i] + fStep*fD1[i];
+    fD2[i] = (fD1[i] - cosi*n[i])*sint/sini + n[i]*cost;
+  } // i
+  ray.SetDirection(fD2);
+
+  // step (m), c (m/s)
+  Double_t speed = TMath::C()*m()/n1;
+  fX2[3] = fX1[3] + fStep/speed;
+  ray.AddPoint(fX2[0], fX2[1], fX2[2], fX2[3]);
+}
+
+//_____________________________________________________________________________
+void AOpticsManager::DoReflection(Double_t n1, ARay& ray)
+{
+  Double_t* n = FindNormal(); // normal vect perpendicular to the surface
+  Double_t cosi = fD1[0]*n[0] + fD1[1]*n[1] + fD1[2]*n[2];
+  for(Int_t i = 0; i < 3; i++){ // d2 = d1 - 2n*(d1*n)
+    fX2[i] = fX1[i] + fStep*fD1[i];
+    fD2[i] = fD1[i] - 2*n[i]*cosi;
+  } // i
+  ray.SetDirection(fD2);
+
+  // step (m), c (m/s)
+  Double_t speed = TMath::C()*m()/n1;
+  fX2[3] = fX1[3] + fStep/speed;
+  ray.AddPoint(fX2[0], fX2[1], fX2[2], fX2[3]);
+}
+
+//_____________________________________________________________________________
 void AOpticsManager::TraceNonSequential(ARay& ray)
 {
   Double_t lambda = ray.GetLambda();
   while(ray.IsRunning()){
-    Double_t x[4]; // start point
-    Double_t nx[3]; // end point
-    Double_t d1[3]; // start direction
-    Double_t d2[3]; // end direction
-    ray.GetLastPoint(x);
-    ray.GetDirection(d1);
-    
-    TGeoNode* startnode = InitTrack(x, d1); // start node
+    //Double_t fX1[4]; // start point
+    //Double_t fX2[3]; // end point
+    //Double_t fD1[3]; // start direction
+    //Double_t fD2[3]; // end direction
+    ray.GetLastPoint(fX1);
+    ray.GetDirection(fD1);
+
+    TGeoNode* startnode = InitTrack(fX1, fD1); // start node
     if(IsOutside()){ // if the current position is outside of top volume
       startnode = 0;
     } // if
 
     TGeoNode* endnode = FindNextBoundaryAndStep();
-    Double_t step = GetStep(); // distance to the next boundary
- 
     enum {kLens, kObs, kMirror, kFocus, kOpt, kOther, kNull};
     Int_t type1, type2;
 
@@ -94,18 +155,22 @@ void AOpticsManager::TraceNonSequential(ARay& ray)
     else if(IsOpticalComponent(endnode)) type2 = kOpt;
     else if(    IsFocalSurface(endnode)) type2 = kFocus;
     else                                 type2 = kOther;
+
+    fStep = GetStep(); // distance to the next boundary
     if(type2 == kMirror){
-      Double_t epsilon = 1e-6; // Fixed in TGeoNavigator.cxx (equiv to 1e-6 cm)
-      step -= epsilon*2; // stop the step before reaching the mirror
+      fStep -= kEpsilon; // make sure that the photon do NOT cross the boundary
+    } else {
+      fStep += kEpsilon; // make sure that the photon crosses the boundary
     } // if
 
     if(type1 == kLens){
       Double_t abs = ((ALens*)startnode->GetVolume())->GetAbsorptionLength(lambda);
       if(abs > 0){
         Double_t abs_step = gRandom->Exp(abs);
-        if(abs_step < step){
-          Double_t speed = (TMath::C()*m())/((ALens*)startnode->GetVolume())->GetRefractiveIndex(ray.GetLambda());
-          ray.AddPoint(x[0] + d1[0]*abs_step, x[1] + d1[1]*abs_step, x[2] + d1[2]*abs_step, x[3] + step/speed);
+        if(abs_step < fStep){
+          Double_t n1 = ((ALens*)startnode->GetVolume())->GetRefractiveIndex(ray.GetLambda());
+          Double_t speed = TMath::C()*m()/n1;
+          ray.AddPoint(fX1[0] + fD1[0]*abs_step, fX1[1] + fD1[1]*abs_step, fX1[2] + fD1[2]*abs_step, fX1[3] + abs_step/speed);
           ray.Stop();
           continue;
         } // if
@@ -114,94 +179,55 @@ void AOpticsManager::TraceNonSequential(ARay& ray)
 
     if((type1 == kNull or type1 == kOpt or type1 == kLens or type1 == kOther)
        and type2 == kMirror){
-
-      Double_t* n = FindNormal(); // normal vect perpendicular to the surface
-      Double_t dn = d1[0]*n[0] + d1[1]*n[1] + d1[2]*n[2];
-      for(Int_t i = 0; i < 3; i++){ // d2 = d1 - 2n*(d1*n)
-        nx[i] = x[i] + step*d1[i];
-        d2[i] = d1[i] - 2*n[i]*dn;
-      } // i
-
-      // step (m), c (m/s)
-      if (type1 == kLens){
-        Double_t speed = (TMath::C()*m())/((ALens*)startnode->GetVolume())->GetRefractiveIndex(ray.GetLambda());
-        ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/speed);
-      } else {
-        ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/(TMath::C()*m()));
-      } // if
-      ray.SetDirection(d2); // reflected
-
+      Double_t n1 = type1 == kLens ? ((ALens*)startnode->GetVolume())->GetRefractiveIndex(ray.GetLambda()) : 1.;
+      DoReflection(n1, ray);
     } else if((type1 == kNull or type1 == kOpt or type1 == kOther)
                and type2 == kLens){
-
-      Double_t* n = FindNormal(); // normal vect perpendicular to the surface
       Double_t n1 = 1; // Assume refractive index equals 1 (= vacuum)
       Double_t n2 = ((ALens*)endnode->GetVolume())->GetRefractiveIndex(ray.GetLambda());
-      Double_t dn = d1[0]*n[0] + d1[1]*n[1] + d1[2]*n[2]; // d1*n
-      Double_t p = TMath::Sqrt(1 - (n1*n1/n2/n2)*(1 - dn*dn));
-      for(Int_t i = 0; i < 3; i++){
-        nx[i] = x[i] + step*d1[i];
-        d2[i] = (d1[i] - dn*n[i])*n1/n2 + n[i]*p;
-      } // i
-      ray.SetDirection(d2); // refracted
-      ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/TMath::C());
-
+      DoFresnel(n1, n2, ray);
     } else if((type1 == kNull or type1 == kLens or type1 == kOpt or type1 == kOther)
                and (type2 == kObs or type2 == kFocus)){
 
       for(Int_t i = 0; i < 3; i++){
-        nx[i] = x[i] + step*d1[i];
+        fX2[i] = fX1[i] + fStep*fD1[i];
       } // i
       if (type1 == kLens){
-        Double_t speed = TMath::C()/((ALens*)startnode->GetVolume())->GetRefractiveIndex(ray.GetLambda());
-        ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/speed);
+        Double_t n1 = ((ALens*)startnode->GetVolume())->GetRefractiveIndex(ray.GetLambda());
+        Double_t speed = TMath::C()*m()/n1;
+        fX2[3] = fX1[3] + fStep/speed;
       } else {
-        ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/TMath::C());
+        Double_t speed = TMath::C()*m();
+        fX2[3] = fX1[3] + fStep/speed;
       } // if
+      ray.AddPoint(fX2[0], fX2[1], fX2[2], fX2[3]);
     } else if((type1 == kNull or type1 == kOpt or type1 == kOther)
                and (type2 == kOther or type2 == kOpt)){
 
       for(Int_t i = 0; i < 3; i++){
-        nx[i] = x[i] + step*d1[i];
+        fX2[i] = fX1[i] + fStep*fD1[i];
       } // i
-      ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/TMath::C());
-
+      Double_t speed = TMath::C()*m();
+      fX2[3] = fX1[3] + fStep/speed;
+      ray.AddPoint(fX2[0], fX2[1], fX2[2], fX2[3]);
     } else if(type1 == kLens and type2 == kLens){
-
-      Double_t* n = FindNormal(); // normal vect perpendicular to the surface
       Double_t n1 = ((ALens*)startnode->GetVolume())->GetRefractiveIndex(lambda);
       Double_t n2 = ((ALens*)endnode->GetVolume())->GetRefractiveIndex(lambda);
-      Double_t dn = d1[0]*n[0] + d1[1]*n[1] + d1[2]*n[2]; // d1*n
-      Double_t p = TMath::Sqrt(1 - (n1*n1/n2/n2)*(1 - dn*dn));
-      for(Int_t i = 0; i < 3; i++){
-        nx[i] = x[i] + step*d1[i];
-        d2[i] = (d1[i] - dn*n[i])*n1/n2 + n[i]*p;
-      } // i
-      ray.SetDirection(d2); // refracted
-      ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/(TMath::C()/n1));
-
+      DoFresnel(n1, n2, ray);
     } else if(type1 == kLens and
               (type2 == kNull or type2 == kOpt or type2 == kOther)){
-
-      Double_t* n = FindNormal(); // normal vect perpendicular to the surface
       Double_t n1 = ((ALens*)startnode->GetVolume())->GetRefractiveIndex(lambda);
       Double_t n2 = 1; // Assume refractive index equals 1 (= vacuum)
-      Double_t dn = d1[0]*n[0] + d1[1]*n[1] + d1[2]*n[2];
-      Double_t p = sqrt(1 - (n1*n1/n2/n2)*(1 - dn*dn));
-      for(Int_t i = 0; i < 3; i++){
-        nx[i] = x[i] + step*d1[i];
-        d2[i] = (d1[i] - dn*n[i])*n1/n2 + n[i]*p;
-      } // i
-      ray.SetDirection(d2); // refracted
-      ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/(TMath::C()/n1));
-
+      DoFresnel(n1, n2, ray);
     } // if
 
     if(type2 == kNull){
       for(Int_t i = 0; i < 3; i++){
-        nx[i] = x[i] + step*d1[i];
+        fX2[i] = fX1[i] + fStep*fD1[i];
       } // i
-      ray.AddPoint(nx[0], nx[1], nx[2], x[3] + step/TMath::C());
+      Double_t speed = TMath::C()*m();
+      fX2[3] = fX1[3] + fStep/speed;
+      ray.AddPoint(fX2[0], fX2[1], fX2[2], fX2[3]);
       ray.Exit();
     } else if(type1 == kFocus or type1 == kObs or type1 == kMirror or type2 == kObs){
       ray.Stop();
