ad1bc3b00666be48653fbeeb2a323152e92b6b78
oxon <oxon@03b345a6-8283-4e7e-8394-dcfc2f156d9d>
2011-12-09 16:26:16 +0000

    Added three different modes

--- tutorial/HexWinstonCone.C
+++ tutorial/HexWinstonCone.C
@@ -15,24 +15,22 @@ static const Double_t um = AOpticsManager::um();
 static const Double_t nm = AOpticsManager::nm();
 static const Double_t  m = AOpticsManager::m();
 
-void HexWinstonCone()
+void HexWinstonCone(Int_t mode = 0)
 {
+  // mode == 0: hex-hex Winston cone built with AGeoWinstonConePoly
+  // mode == 1: hex-hex Winston cone built with three AGeoWinstonCone2D
+  // mode == 2: circle-circle Winston cone with AGeoWinstonConePoly
+
   AOpticsManager* manager = new AOpticsManager("manager", "SC");
-  //manager->SetNsegments(100);
 
   // Make the world
   TGeoBBox* worldbox = new TGeoBBox("worldbox", 30*m, 30*m, 30*m);
   AOpticalComponent* world = new AOpticalComponent("world", worldbox);
   manager->SetTopVolume(world);
 
-  // Top volume 
-  TGeoBBox* topbox = new TGeoBBox("topbox", 30*m, 30*m, 30*m);
-  AOpticalComponent* top = new AOpticalComponent("top", topbox);
-
   // Make the primary mirror
-  Double_t rin = 20*mm;
-  Double_t rout = 10*mm;
-  AGeoWinstonCone2D* coneV = new AGeoWinstonCone2D("coneV", rin, rout, rin*1.733);
+  const Double_t kRin = 20*mm;
+  const Double_t kRout = 10*mm;
   TGeoRotation* rot30 = new TGeoRotation("rot30", 30, 0, 0);
   rot30->RegisterYourself();
   TGeoRotation* rot60 = new TGeoRotation("rot60", 60, 0, 0);
@@ -40,30 +38,40 @@ void HexWinstonCone()
   TGeoRotation* rot120 = new TGeoRotation("rot120", 120, 0, 0);
   rot120->RegisterYourself();
 
+  AGeoWinstonCone2D* coneV = new AGeoWinstonCone2D("coneV", kRin, kRout, kRin*1.733);
   TGeoPgon* pgon = new TGeoPgon("pgon", 0, 360, 6, 4);
-  pgon->DefineSection(0, -coneV->GetDZ()*0.999, 0, rout*1.001);
-  pgon->DefineSection(1, -coneV->GetDZ()*0.5, 0, rin*0.9);
-  pgon->DefineSection(2, -coneV->GetDZ()*0., 0, rin*0.99);
-  pgon->DefineSection(3,  coneV->GetDZ()*0.999, 0, rin*1.001);
+  pgon->DefineSection(0, -coneV->GetDZ()*0.999, 0, kRout*1.001);
+  pgon->DefineSection(1, -coneV->GetDZ()*0.5, 0, kRin*0.9);
+  pgon->DefineSection(2, -coneV->GetDZ()*0., 0, kRin*0.99);
+  pgon->DefineSection(3,  coneV->GetDZ()*0.999, 0, kRin*1.001);
 
-  TGeoCompositeShape* coneComp1 = new TGeoCompositeShape("coneComp1", "coneV*(coneV:rot60)*(coneV:rot120)");
-  TGeoCompositeShape* coneComp2 = new TGeoCompositeShape("coneComp2", "pgon:rot30 - coneComp1");
-  //  TGeoCompositeShape* coneComp2 = new TGeoCompositeShape("coneComp2", "pgon:rot30 - coneV");
-  AMirror* coneMirror = new AMirror("coneMirror", coneComp2);
-  top->AddNode(coneMirror, 1);
+  if(mode == 0){
+    AGeoWinstonConePoly* hexV = new AGeoWinstonConePoly("hexV", kRin, kRout, 6);
+    TGeoCompositeShape* coneComp1 = new TGeoCompositeShape("coneComp1", "pgon:rot30 - hexV");
+    AMirror* coneMirror = new AMirror("coneMirror", coneComp1);
+    world->AddNode(coneMirror, 1);
+  } else if(mode == 1){
+    TGeoCompositeShape* coneComp1 = new TGeoCompositeShape("coneComp1", "coneV*(coneV:rot60)*(coneV:rot120)");
+    TGeoCompositeShape* coneComp2 = new TGeoCompositeShape("coneComp2", "pgon:rot30 - coneComp1");
+    AMirror* coneMirror = new AMirror("coneMirror", coneComp2);
+    world->AddNode(coneMirror, 1);
+  } else {
+    AGeoWinstonConePoly* hexV = new AGeoWinstonConePoly("hexV", kRin, kRout, 100);
+    TGeoCompositeShape* coneComp1 = new TGeoCompositeShape("coneComp1", "pgon:rot30 - hexV");
+    AMirror* coneMirror = new AMirror("coneMirror", coneComp1);
+    world->AddNode(coneMirror, 1);
+  } // if
 
   TGeoPgon* pgonPMT = new TGeoPgon("pgonPMT", 0, 360, 6, 2);
-  pgonPMT->DefineSection(0, -coneV->GetDZ() - 0.01*mm, 0, rout*1.01);
-  pgonPMT->DefineSection(1, -coneV->GetDZ()       , 0, rout*1.01);
+  pgonPMT->DefineSection(0, -coneV->GetDZ() - 0.01*mm, 0, kRout*1.01);
+  pgonPMT->DefineSection(1, -coneV->GetDZ()       , 0, kRout*1.01);
   AFocalSurface* pmt = new AFocalSurface("pmt", pgonPMT);
-  top->AddNode(pmt, 1, rot30);
-
-  world->AddNode(top, 1);
+  world->AddNode(pmt, 1, rot30);
 
   manager->CloseGeometry();
 
   TCanvas* can1 = new TCanvas("can1", "can1");
-  top->Draw();
+  world->Draw();
 
   TGraph* graAeff = new TGraph;
 
@@ -73,9 +81,9 @@ void HexWinstonCone()
 
     TVector3 dir(0, 0, 1);
     Double_t lambda = 400*nm; // does not affect the results because we have no lens
-    // 1 photon per 0.0625 mm^2
+    // 1 photon per 1 mm^2
     ARayArray* array = ARayShooter::Square(lambda, 100*mm, 101, rayrot, raytr, &dir);
-    //    ARayArray* array = ARayShooter::Rectangle(lambda, 1*mm, 100*mm, 2, 501, rayrot, raytr, &dir);
+    Double_t dA = 1*mm*mm;
     manager->TraceNonSequential(*array);
     TObjArray* focused = array->GetFocused();
 
@@ -85,20 +93,25 @@ void HexWinstonCone()
       if(!ray) continue;
 
       // Calculate the effective area from the number of focused photons
-      Aeff += 0.0625; // 0.0625 (mm^2)
+      Aeff += dA;
 
-      if(graAeff->GetN() == 290){
-	ray->Draw(); // high CPU load!
+      if(graAeff->GetN() == 0){
+        ray->Draw(); // high CPU load!
       } // if
     } // j
 
-    graAeff->SetPoint(graAeff->GetN(), deg, Aeff);
+    if(mode == 0 || mode == 1){
+      graAeff->SetPoint(graAeff->GetN(), deg, Aeff/(2*TMath::Sqrt(3)*kRin*kRin)/TMath::Cos(deg*TMath::DegToRad()));
+    } else {
+      graAeff->SetPoint(graAeff->GetN(), deg, Aeff/(TMath::Pi()*kRin*kRin)/TMath::Cos(deg*TMath::DegToRad()));
+    } // if
+
 
     delete array;
   } // deg
 
   TCanvas* can2 = new TCanvas("can2", "can2");
+  graAeff->GetXaxis()->SetTitle("Incident Angle (deg)");
+  graAeff->GetYaxis()->SetTitle("Collection Efficiency (%)");
   graAeff->Draw("ap");
-
-  //  coneMirror->Raytrace();
 }
